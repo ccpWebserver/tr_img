429a8c6abc6b152acb59111134322dda42e6fdda
Your Name <you@example.com>
2024-10-29 17:58:04 +0800

    车辆入场添加判断条件

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: [ "antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table" ],
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+  publicRuntimeConfig: {
+    END_POINT: process.env.END_POINT,
+  },
+  compress: true,
+  output: 'standalone',
 };
 
 export default nextConfig;
